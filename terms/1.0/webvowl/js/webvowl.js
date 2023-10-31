389c0b1555a10c67e2cdf70cf547a7f6e2f5b36a
Maximvdw <Maximvdw@users.noreply.github.com>
2023-10-31 14:13:27 +0000

    deploy: c6efaa4720cdaecb4c1e19a1134aadc7eaae6bb3

--- terms/1.0/webvowl/js/webvowl.js
+++ terms/1.0/webvowl/js/webvowl.js
@@ -21021,4 +21021,37 @@ webvowl =
 	 */
 	function memoizeCapped(func) {
 	  var result = memoize(func, function(key) {
-	    if (c
+	    if (cache.size === MAX_MEMOIZE_SIZE) {
+	      cache.clear();
+	    }
+	    return key;
+	  });
+
+	  var cache = result.cache;
+	  return result;
+	}
+
+	module.exports = memoizeCapped;
+
+
+/***/ }),
+/* 218 */
+/***/ (function(module, exports, __webpack_require__) {
+
+	var MapCache = __webpack_require__(117);
+
+	/** Error message constants. */
+	var FUNC_ERROR_TEXT = 'Expected a function';
+
+	/**
+	 * Creates a function that memoizes the result of `func`. If `resolver` is
+	 * provided, it determines the cache key for storing the result based on the
+	 * arguments provided to the memoized function. By default, the first argument
+	 * provided to the memoized function is used as the map cache key. The `func`
+	 * is invoked with the `this` binding of the memoized function.
+	 *
+	 * **Note:** The cache is exposed as the `cache` property on the memoized
+	 * function. Its creation may be customized by replacing the `_.memoize.Cache`
+	 * constructor with one whose instances implement the
+	 * [`Map`](http://ecma-international.org/ecma-262/7.0/#sec-properties-of-the-map-prototype-object)
+	 * method interface of `clear`, `delete`, `get`, `has`, and `s
